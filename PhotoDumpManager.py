ac098a92c655769fdbd4364a9063b33589e27f65
Benjamin BEAU <beau.benjamin.17@gmail.com>
2019-04-15 13:51:08 +0200

    adding file type sort

--- PhotoDumpManager.py
+++ PhotoDumpManager.py
@@ -6,20 +6,28 @@ if(len(sys.argv) < 3):
 
 dumpDirectory = sys.argv[1]
 exitDirectory = sys.argv[2]
-filepath = ''
+filePath = ''
+fileExt = ''
 
 if not os.path.exists(exitDirectory):
     raise ValueError('No directory at '+dumpDirectory+'.')
 if not os.path.exists(exitDirectory):
     raise ValueError('No directory at '+exitDirectory+'.')
 
+if not dumpDirectory[-1:] == os.sep:
+    dumpDirectory += os.sep
+
+if not exitDirectory[-1:] == os.sep:
+    exitDirectory += os.sep
+
 for filename in os.listdir(dumpDirectory):
-    test = time.strftime("%Y-%B-%d", time.localtime(os.path.getctime(dumpDirectory+filename))).split('-')
-    filepath = exitDirectory
-    for i in range(3):
-        filepath += test[i] + '/'
-        if not os.path.exists(filepath):
-            os.makedirs(filepath)
-    if not os.path.exists(filepath + filename):
-        copy2(dumpDirectory + filename, filepath + filename)
+    test = time.strftime("%Y-%B-%d", time.localtime(os.path.getmtime(dumpDirectory+filename))).split('-')
+    test.insert(0, filename.split('.')[1])
+    filePath = exitDirectory
+    for text in test:
+        filePath += text + os.sep
+        if not os.path.exists(filePath):
+            os.makedirs(filePath)
+    if not os.path.exists(filePath + filename):
+        copy2(dumpDirectory + filename, filePath + filename)
     
