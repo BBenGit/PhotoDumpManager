2f2c90000980a9b4234a2547434e635b9875ef48
Benjamin BEAU <beau.benjamin.17@gmail.com>
2019-04-15 11:52:19 +0200

    finishing script, todo test

--- PhotoDumpManager.py
+++ PhotoDumpManager.py
@@ -1,8 +1,25 @@
-import os.path
-import os, time
+import os.path, os, time, sys
+from shutil import copy2
 
-dumpDirectory = '/Users/bbeau/Pictures/Dump/RAW/'
-exitDirectory = '/Users/bbeau/Downloads/test/'
+if(len(sys.argv) < 3):
+    raise AttributeError('Please provide path for dump and exit directories.')
+
+dumpDirectory = sys.argv[1]
+exitDirectory = sys.argv[2]
+filepath = ''
+
+if not os.path.exists(exitDirectory):
+    raise ValueError('No directory at '+dumpDirectory+'.')
+if not os.path.exists(exitDirectory):
+    raise ValueError('No directory at '+exitDirectory+'.')
 
 for filename in os.listdir(dumpDirectory):
-    print(time.ctime(os.path.getmtime(dumpDirectory+filename)))
+    test = time.strftime("%Y-%B-%d", time.localtime(os.path.getctime(dumpDirectory+filename))).split('-')
+    filepath = exitDirectory
+    for i in range(3):
+        filepath += test[i] + '/'
+        if not os.path.exists(filepath):
+            os.makedirs(filepath)
+    if not os.path.exists(filepath + filename):
+        copy2(dumpDirectory + filename, filepath + filename)
+    
