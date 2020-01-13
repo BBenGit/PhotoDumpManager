c6feeb00318d6e97e99087c3f7f7084455c4262d
Benjamin Beau <fnounfoun@gmail.com>
2020-01-13 10:40:19 +0100

    fix/faulty signature

--- PhotoDumpManager.py
+++ PhotoDumpManager.py
@@ -62,7 +62,7 @@ def sort(inner_directory, output_directory, types):
                         iterator += 1
             else:
                 if recursive:
-                    sort(os.path.join(inner_directory, filename), output_directory, types, recursive)
+                    sort(os.path.join(inner_directory, filename), output_directory, types)
 
 def file_count(inner_directory, output_directory, types):
     global count
@@ -84,7 +84,7 @@ def file_count(inner_directory, output_directory, types):
 
         else:
             if recursive:
-                file_count(os.path.join(inner_directory, filename), output_directory, types, recursive)
+                file_count(os.path.join(inner_directory, filename), output_directory, types)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
