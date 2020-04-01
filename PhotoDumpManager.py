bc8a129017990c7582dfd80f5b4080d0298bdf18
Ben <fnounfoun@gmail.com>
2020-04-01 18:58:26 +0200

    one file

--- PhotoDumpManager.py
+++ PhotoDumpManager.py
@@ -24,7 +24,59 @@ import subprocess
 from datetime import datetime
 from pathlib import Path
 from shutil import copy2
-from utils.log import init_logger
+import colorlog
+import logging
+
+
+def init_logger(dunder_name, testing_mode) -> logging.Logger:
+    log_format = (
+        '%(asctime)s - '
+        '%(funcName)s - '
+        '%(levelname)s - '
+        '%(message)s'
+    )
+    colorlog_format = (
+        '%(white)s'
+        '%(asctime)s - '
+        '%(log_color)s'
+        '%(levelname)-8s'
+        '%(white)s - '
+        '%(cyan)s'
+        '%(funcName)s'
+        '%(white)s - '
+        '%(message)s'
+    )
+    colorlog.basicConfig(format=colorlog_format)
+    logger = logging.getLogger(dunder_name)
+
+    if testing_mode:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
+    # Output full log
+    fh = logging.FileHandler('app.log')
+    fh.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(log_format)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    # Output warning log
+    fh = logging.FileHandler('app.warning.log')
+    fh.setLevel(logging.WARNING)
+    formatter = logging.Formatter(log_format)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    # Output error log
+    fh = logging.FileHandler('app.error.log')
+    fh.setLevel(logging.ERROR)
+    formatter = logging.Formatter(log_format)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    return logger
+
 
 logger = init_logger(__name__, testing_mode=False)
 month_name = ["1 - Janvier", "2 - Février", "3 - Mars", "4 - Avril", "5 - Mai", "6 - Juin", "7 - Juillet",
@@ -34,7 +86,8 @@ image_types = ["jpg", "jpeg", "png", "raw", "nef", "dng", "insp"]
 
 
 def get_file_name(create_datetime, extension, add_microsecond):
-    filename = str(create_datetime.date()) + "_" + str(create_datetime.time()).replace(":", "")
+    filename = str(create_datetime.date()) + "_" + \
+        str(create_datetime.time()).replace(":", "")
 
     if add_microsecond < 10:
         filename = filename + "000"
@@ -49,7 +102,8 @@ def get_file_tags(path):
         try:
             tags = exifread.process_file(file)
         except TypeError:
-            logger.error("TypeError while getting files tags for file {}".format(path))
+            logger.error(
+                "TypeError while getting files tags for file {}".format(path))
             tags = {}
 
         file.close()
@@ -62,23 +116,28 @@ def get_tag(tags, tag_name):
         tag = tags[tag_name]
         string = str(tag)
     except KeyError:
-        logger.error("KeyError for '{}'. Tags list = {}".format(tag_name, tags))
+        logger.error(
+            "KeyError for '{}'. Tags list = {}".format(tag_name, tags))
     finally:
         return string
 
 
 def get_image_create_datetime(path):
-    datetime_original = get_cmd_output('exiftool -DateTimeOriginal "' + path + '"', "Date/Time Original")
-    create_date = get_cmd_output('exiftool -CreateDate "' + path + '"', "Create Date")
+    datetime_original = get_cmd_output(
+        'exiftool -DateTimeOriginal "' + path + '"', "Date/Time Original")
+    create_date = get_cmd_output(
+        'exiftool -CreateDate "' + path + '"', "Create Date")
 
     if datetime_original != "":
         logger.info("Use metadata datetime from tag : DateTimeOriginal")
         test = str(datetime_original).split(' ')
-        create_datetime = datetime.fromisoformat(test[0].replace(':', '-') + 'T' + test[1])
+        create_datetime = datetime.fromisoformat(
+            test[0].replace(':', '-') + 'T' + test[1])
     elif create_date != "":
         logger.info("Use metadata datetime from tag : CreateDate")
         test = str(create_date).split(' ')
-        create_datetime = datetime.fromisoformat(test[0].replace(':', '-') + 'T' + test[1])
+        create_datetime = datetime.fromisoformat(
+            test[0].replace(':', '-') + 'T' + test[1])
     else:
         create_datetime = get_file_modification_datetime(path)
 
@@ -89,7 +148,8 @@ def get_cmd_output(cmd, string_prefix):
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
 
-    result = str(output).replace("b'" + string_prefix, "").replace("'", "").replace("\\n", "")
+    result = str(output).replace("b'" + string_prefix,
+                                 "").replace("'", "").replace("\\n", "")
     str_splitted = result.split(' : ')
     if len(str_splitted) > 1:
         return str_splitted[1]
@@ -99,8 +159,10 @@ def get_cmd_output(cmd, string_prefix):
 
 def get_file_modification_datetime(path):
     logger.info("ctime = {}, mtime = {}, atime = {}".format(
-        datetime.fromtimestamp(time.mktime(time.localtime(os.path.getctime(path)))),
-        datetime.fromtimestamp(time.mktime(time.localtime(os.path.getmtime(path)))),
+        datetime.fromtimestamp(time.mktime(
+            time.localtime(os.path.getctime(path)))),
+        datetime.fromtimestamp(time.mktime(
+            time.localtime(os.path.getmtime(path)))),
         datetime.fromtimestamp(time.mktime(time.localtime(os.path.getatime(path))))))
     logger.info("Use file modification datetime...")
     return datetime.fromtimestamp(time.mktime(time.localtime(os.path.getmtime(path))))
@@ -115,17 +177,21 @@ def sort(inner_directory, output_directory, recursive):
         if not filename.startswith('.'):  # Ignore hidden files
             if os.path.isfile(os.path.join(inner_directory, filename)):
                 with open(os.path.join(inner_directory, filename), 'rb') as file:
-                    logger.info("Reading file {}...".format(os.path.join(inner_directory, filename)))
+                    logger.info("Reading file {}...".format(
+                        os.path.join(inner_directory, filename)))
 
-                    extension = Path(filename).suffix[1:]  # Remove trailing point between filename and extension
+                    # Remove trailing point between filename and extension
+                    extension = Path(filename).suffix[1:]
 
                     do_sorting = False
 
                     if extension.lower() in image_types:
-                        access_time = get_image_create_datetime(os.path.join(inner_directory, filename))
+                        access_time = get_image_create_datetime(
+                            os.path.join(inner_directory, filename))
                         do_sorting = True
                     elif extension.lower() in video_types:
-                        access_time = get_video_create_datetime(os.path.join(inner_directory, filename))
+                        access_time = get_video_create_datetime(
+                            os.path.join(inner_directory, filename))
                         do_sorting = True
                     else:
                         logger.error("Error: file extension unknown for file {}"
@@ -135,7 +201,8 @@ def sort(inner_directory, output_directory, recursive):
                         # Make subdirectory like: filename/year/month/day
                         file_path = output_directory
                         for subdir_name in (access_time.year, month_name[access_time.month - 1], access_time.day):
-                            file_path = os.path.join(file_path, str(subdir_name))
+                            file_path = os.path.join(
+                                file_path, str(subdir_name))
                             if not os.path.exists(file_path):
                                 os.makedirs(file_path)
 
@@ -146,16 +213,19 @@ def sort(inner_directory, output_directory, recursive):
                         # Copy the image if does not already exists
                         while os.path.exists(os.path.join(file_path, new_filename)):
                             i = i + 1
-                            new_filename = get_file_name(access_time, extension, i)
+                            new_filename = get_file_name(
+                                access_time, extension, i)
 
                         input_file, output_file = os.path.join(inner_directory, filename), os.path.join(
                             file_path,
                             new_filename)
-                        logger.info("Copying {} to {}".format(input_file, output_file))
+                        logger.info("Copying {} to {}".format(
+                            input_file, output_file))
                         copy2(input_file, output_file)
             else:
                 if recursive & (os.path.join(inner_directory, filename) != output_directory):
-                    sort(os.path.join(inner_directory, filename), output_directory, recursive)
+                    sort(os.path.join(inner_directory, filename),
+                         output_directory, recursive)
 
 
 def batch_rename(inner_directory, output_directory, date):
@@ -210,14 +280,16 @@ def update_timezone_metadata(inner_directory, timezone, timezone_sign):
                 else:
                     logger.info("File {} is not an image".format(filename))
             else:
-                update_timezone_metadata(os.path.join(inner_directory, filename), timezone, timezone_sign)
+                update_timezone_metadata(os.path.join(
+                    inner_directory, filename), timezone, timezone_sign)
 
 
 def check_directories(input_directory, output_directory):
     if not os.path.exists(input_directory):
         raise NotADirectoryError('No directory at %s.', input_directory)
     if not os.path.exists(output_directory):
-        logger.info("Missing output directory at %s. Creating it…", output_directory)
+        logger.info("Missing output directory at %s. Creating it…",
+                    output_directory)
         os.makedirs(output_directory)
 
 
@@ -230,12 +302,13 @@ def shift_all_date(input_directory, shift_sign, shift_string):
                 if extension.lower() in image_types:
 
                     shift_cmd = 'exiftool "-AllDates' + shift_sign + '=' + shift_string + '" -overwrite_original -m "' \
-                          + os.path.join(input_directory, filename) + '"'
+                        + os.path.join(input_directory, filename) + '"'
                     os.system(shift_cmd)
                 else:
                     logger.info("File {} is not an image".format(filename))
             else:
-                shift_all_date(os.path.join(input_directory, filename), shift_sign, shift_string)
+                shift_all_date(os.path.join(input_directory,
+                                            filename), shift_sign, shift_string)
 
 
 if __name__ == "__main__":
@@ -253,20 +326,23 @@ if __name__ == "__main__":
     elif choice == "r":
         input_directory = input("Select directory to batch rename files: ")
         output_directory = input_directory + "new"
-        date = datetime.fromisoformat(input("Select date to rename file (YYYY-MM-DDTHH:MM:SS): "))
+        date = datetime.fromisoformat(
+            input("Select date to rename file (YYYY-MM-DDTHH:MM:SS): "))
 
         check_directories(input_directory, output_directory)
         batch_rename(input_directory, output_directory, date)
 
     elif choice == "t":
-        input_directory = input("Select directory of your files you want to change timezone metadata: ")
+        input_directory = input(
+            "Select directory of your files you want to change timezone metadata: ")
         timezone = input("Enter your timezone value (eg: 4): ")
         timezone_sign = input("Enter your timezone sign (eg: -/+): ")
         update_timezone_metadata(input_directory, timezone, timezone_sign)
         logger.info("Nb image updated = {}".format(COUNT))
 
     elif choice == "sd":
-        input_directory = input("Select directory of your files you want to shift time metadata: ")
+        input_directory = input(
+            "Select directory of your files you want to shift time metadata: ")
         shift_sign = input("Add (+) or remove (-) time:")
         shift_string = input("Enter the shift string (eg: Y:MM:DD TT:MM:SS): ")
         shift_all_date(input_directory, shift_sign, shift_string)
@@ -277,6 +353,9 @@ if __name__ == "__main__":
               "image_to_overwrite.jpg")
 
     logger.info("")
-    logger.info("----------------------------------------------------------------------------")
-    logger.info("----------------------               END             -----------------------")
-    logger.info("----------------------------------------------------------------------------")
+    logger.info(
+        "----------------------------------------------------------------------------")
+    logger.info(
+        "----------------------               END             -----------------------")
+    logger.info(
+        "----------------------------------------------------------------------------")
